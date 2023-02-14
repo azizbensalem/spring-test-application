e2fe2cffcf0e313139d7cff22a7d3b8286e1da8f
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-02-14 15:22:30 +0100

    first commit

---
+++ src/test/java/pfe/talan/test/test_application/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package pfe.talan.test.test_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
