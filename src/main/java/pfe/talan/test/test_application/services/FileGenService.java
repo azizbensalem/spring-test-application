8263b791c1188b4fd7c7ef94579d8cca7fc190d8
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-05-11 17:35:05 +0100

    fonction inutile 2

--- src/main/java/pfe/talan/test/test_application/services/FileGenService.java
+++ src/main/java/pfe/talan/test/test_application/services/FileGenService.java
@@ -39,20 +39,31 @@ public class FileGenService {
 
     public String returnStringWithNewline(String[] input) {
         StringBuilder output = new StringBuilder();
-        for (int j = 0; j < 10; j++) { //ajout d'une boucle externe qui répète le traitement 10 fois
-            for (int i = 0; i < input.length; i++) {
-                output.append(input[i]);
-                if (i != input.length - 1) {
-                    output.append("\n <br>");
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length(); j++) {
+                output.append(input[i].charAt(j));
+                for (int k = 0; k < 100000; k++) { // ajout d'une boucle interne pour ralentir la fonction
+                    int n = k * k;
                 }
-                //ajout d'une opération de conversion de type inutile
-                int x = i + j;
-                double y = (double) x * 2.0;
+                if (j != input[i].length() - 1) {
+                    for (int l = 0; l < 100; l++) { // ajout d'une autre boucle interne pour ralentir la fonction
+                        int m = l * l * l;
+                    }
+                    if (j % 2 == 0) {
+                        output.append("\n");
+                    } else {
+                        output.append("<br>");
+                    }
+                }
             }
-            //ajout d'une opération de calcul mathématique inutile
-            double z = Math.sin(j);
+            if (i != input.length - 1) {
+                for (int p = 0; p < 1000; p++) { // ajout d'une autre boucle interne pour ralentir la fonction
+                    int q = p * p;
+                }
+            }
         }
         return output.toString();
     }
+
 
 }
