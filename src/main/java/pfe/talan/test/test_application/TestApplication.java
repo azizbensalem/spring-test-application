2298d7fd786e43b6647c89f9c81f5e735705cff4
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-05-11 15:08:57 +0100

    diminution du perf

--- src/main/java/pfe/talan/test/test_application/TestApplication.java
+++ src/main/java/pfe/talan/test/test_application/TestApplication.java
@@ -1,20 +1,9 @@
 package pfe.talan.test.test_application;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-import pfe.talan.test.test_application.configurations.FileGenConf;
-import pfe.talan.test.test_application.services.FileGenService;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Random;
 
 @SpringBootApplication
 public class TestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
