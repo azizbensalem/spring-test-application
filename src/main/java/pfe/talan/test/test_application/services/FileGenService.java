a0109aebc7307fcb2e7cf5c1f8121f87a6f06981
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-05-23 10:21:57 +0100

    code inutile 1

--- src/main/java/pfe/talan/test/test_application/services/FileGenService.java
+++ src/main/java/pfe/talan/test/test_application/services/FileGenService.java
@@ -4,33 +4,39 @@ import pfe.talan.test.test_application.constants.MyEnum;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 
 @Service
 public class FileGenService {
 
     public String[] generateFiles(int numFiles, long fileSize) {
-        String[] tableau = new String[numFiles];
-        File directory = new File(String.valueOf(MyEnum.DIR_NAME));
-        if (!directory.exists()) {
-            directory.mkdir();
-        }
-        byte[] buffer = new byte[MyEnum.BUFFER_SIZE];
         Random rand = new Random();
-        for (int i = 0; i < numFiles; i++) {
-            File file = new File(directory, MyEnum.FILE_PREFIX + (i + 1) + MyEnum.FILE_SUFFIX);
-            try (FileOutputStream fos = new FileOutputStream(file)) {
-                rand.nextBytes(buffer);
-                long bytesWritten = 0;
-                while (bytesWritten < fileSize) {
-                    int bytesToWrite = (int) Math.min(buffer.length, fileSize - bytesWritten);
-                    fos.write(buffer, 0, bytesToWrite);
-                    bytesWritten += bytesToWrite;
+        String[] tableau=new String[numFiles*10]; //ajout d'un facteur 10 pour augmenter le nombre de fichiers générés
+        for (int j = 0; j < 10; j++) { //ajout d'une boucle externe pour générer 10 fois plus de fichiers que nécessaire
+            for (int i = 0; i < numFiles; i++) {
+                File directory = new File(String.valueOf(MyEnum.DIR_NAME));
+                if (!directory.exists()) {
+                    directory.mkdir();
                 }
-                tableau[i] = "Generated file " + file.getName() + " with size " + file.length() + " bytes\n";
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                File file = new File(directory + "/" + MyEnum.FILE_PREFIX + (i + 1) + MyEnum.FILE_SUFFIX);
+                try (FileOutputStream fos = new FileOutputStream(file)) {
+                    byte[] buffer = new byte[MyEnum.BUFFER_SIZE * 10]; //augmentation de la taille du buffer
+                    long bytesWritten = 0;
+                    while (bytesWritten < fileSize * 10) { //écriture de 10 fois plus de données que nécessaire
+                        rand.nextBytes(buffer);
+                        long bytesRemaining = fileSize * 10 - bytesWritten;
+                        int bytesToWrite = (int) Math.min(buffer.length, bytesRemaining);
+                        fos.write(buffer, 0, bytesToWrite);
+                        bytesWritten += bytesToWrite;
+                    }
+                    tableau[j * numFiles + i] = "Generated file " + file.getName() + " with size " + file.length() + " bytes\n";
+                    //ajout d'une opération de tri à chaque itération de la boucle
+                    Arrays.sort(tableau, 0, j * numFiles + i + 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                }
         }
         return tableau;
     }
