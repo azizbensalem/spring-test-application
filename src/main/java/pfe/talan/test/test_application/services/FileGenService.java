55b2cc499fa21b21213e46256fa5257813d8295e
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-05-25 14:13:21 +0100

    code inutile 2

--- src/main/java/pfe/talan/test/test_application/services/FileGenService.java
+++ src/main/java/pfe/talan/test/test_application/services/FileGenService.java
@@ -12,39 +12,44 @@ public class FileGenService {
 
     public String[] generateFiles(int numFiles, long fileSize) {
         Random rand = new Random();
-        String[] tableau=new String[numFiles*10]; //ajout d'un facteur 10 pour augmenter le nombre de fichiers générés
-        for (int j = 0; j < 10; j++) { //ajout d'une boucle externe pour générer 10 fois plus de fichiers que nécessaire
-            for (int i = 0; i < numFiles; i++) {
-                File directory = new File(String.valueOf(MyEnum.DIR_NAME));
-                if (!directory.exists()) {
-                    directory.mkdir();
+        String[] tableau = new String[numFiles];
+        File directory = new File(String.valueOf(MyEnum.DIR_NAME));
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        byte[] buffer = new byte[MyEnum.BUFFER_SIZE];
+        for (int i = 0; i < numFiles; i++) {
+            File file = new File(directory, MyEnum.FILE_PREFIX + (i + 1) + MyEnum.FILE_SUFFIX);
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                rand.nextBytes(buffer);
+                long bytesWritten = 0;
+                while (bytesWritten < fileSize) {
+                    int bytesToWrite = (int) Math.min(buffer.length, fileSize - bytesWritten);
+                    fos.write(buffer, 0, bytesToWrite);
+                    bytesWritten += bytesToWrite;
                 }
-                File file = new File(directory + "/" + MyEnum.FILE_PREFIX + (i + 1) + MyEnum.FILE_SUFFIX);
-                try (FileOutputStream fos = new FileOutputStream(file)) {
-                    byte[] buffer = new byte[MyEnum.BUFFER_SIZE * 10]; //augmentation de la taille du buffer
-                    long bytesWritten = 0;
-                    while (bytesWritten < fileSize * 10) { //écriture de 10 fois plus de données que nécessaire
-                        rand.nextBytes(buffer);
-                        long bytesRemaining = fileSize * 10 - bytesWritten;
-                        int bytesToWrite = (int) Math.min(buffer.length, bytesRemaining);
-                        fos.write(buffer, 0, bytesToWrite);
-                        bytesWritten += bytesToWrite;
-                    }
-                    tableau[j * numFiles + i] = "Generated file " + file.getName() + " with size " + file.length() + " bytes\n";
-                    //ajout d'une opération de tri à chaque itération de la boucle
-                    Arrays.sort(tableau, 0, j * numFiles + i + 1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                }
+                tableau[i] = "Generated file " + file.getName() + " with size " + file.length() + " bytes\n";
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return tableau;
     }
 
     public String returnStringWithNewline(String[] input) {
         StringBuilder output = new StringBuilder(input.length * (input.length + 1));
-        for (String str : input) {
-            output.append(str).append("\n <br>");
+        for (int j = 0; j < 10; j++) { //ajout d'une boucle externe qui répète le traitement 10 fois
+            for (int i = 0; i < input.length; i++) {
+                output.append(input[i]);
+                if (i != input.length - 1) {
+                    output.append("\n <br>");
+                }
+                //ajout d'une opération de conversion de type inutile
+                int x = i + j;
+                double y = (double) x * 2.0;
+            }
+            //ajout d'une opération de calcul mathématique inutile
+            double z = Math.sin(j);
         }
         return output.toString();
     }
