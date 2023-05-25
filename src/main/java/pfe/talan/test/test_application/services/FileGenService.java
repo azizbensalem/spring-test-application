6d31386f8e1c6c0c7e718806e7fdb582c2a28102
Aziz Ben Salem <azizbensalem1998@gmail.com>
2023-05-25 16:27:12 +0100

    code utile 1

--- src/main/java/pfe/talan/test/test_application/services/FileGenService.java
+++ src/main/java/pfe/talan/test/test_application/services/FileGenService.java
@@ -11,13 +11,13 @@ import java.util.Random;
 public class FileGenService {
 
     public String[] generateFiles(int numFiles, long fileSize) {
-        Random rand = new Random();
         String[] tableau = new String[numFiles];
         File directory = new File(String.valueOf(MyEnum.DIR_NAME));
         if (!directory.exists()) {
             directory.mkdir();
         }
         byte[] buffer = new byte[MyEnum.BUFFER_SIZE];
+        Random rand = new Random();
         for (int i = 0; i < numFiles; i++) {
             File file = new File(directory, MyEnum.FILE_PREFIX + (i + 1) + MyEnum.FILE_SUFFIX);
             try (FileOutputStream fos = new FileOutputStream(file)) {
@@ -29,6 +29,7 @@ public class FileGenService {
                     bytesWritten += bytesToWrite;
                 }
                 tableau[i] = "Generated file " + file.getName() + " with size " + file.length() + " bytes\n";
+                //System.out.println("Generated file " + file.getName() + " with size " + file.length() + " bytes");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -38,18 +39,8 @@ public class FileGenService {
 
     public String returnStringWithNewline(String[] input) {
         StringBuilder output = new StringBuilder(input.length * (input.length + 1));
-        for (int j = 0; j < 10; j++) { //ajout d'une boucle externe qui répète le traitement 10 fois
-            for (int i = 0; i < input.length; i++) {
-                output.append(input[i]);
-                if (i != input.length - 1) {
-                    output.append("\n <br>");
-                }
-                //ajout d'une opération de conversion de type inutile
-                int x = i + j;
-                double y = (double) x * 2.0;
-            }
-            //ajout d'une opération de calcul mathématique inutile
-            double z = Math.sin(j);
+        for (String str : input) {
+            output.append(str).append("\n <br>");
         }
         return output.toString();
     }
